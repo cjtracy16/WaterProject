18c0806b1af6d4a1bf21f706759cd594e7cf85af
cjtracy <connertracy@comcast.net>
2022-02-14 19:01:29 -0700

    Database

---
+++ WaterProject/WaterProject/Models/Project.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaterProject.Models
+{
+    public class Project
+    {
+        [Key]
+        [Required]
+        public int MovieId { get; set; }
+
+        //[Required(ErrorMessage = "")]
+        public string ProjectName { get; set; }
+
+        //[Required(ErrorMessage = "")]
+        public string ProjectType { get; set; }
+
+       // [Required(ErrorMessage = "")]
+        public string ProjectRegionalProgram { get; set; }
+
+        //[Required(ErrorMessage = "")]
+        public int ProjectImpact { get; set; }
+
+        //[Required(ErrorMessage = "")]
+        public string ProjectPhase { get; set; }
+
+        //[Required(ErrorMessage = "")]
+        public string ProjectFunctionalityStatus { get; set; }
+
+    }
+}
